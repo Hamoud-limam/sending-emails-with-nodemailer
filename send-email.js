9cf1e151189f2bef7e410581002924c3b15bbbd2
Hamoud-Limam <hamoudlimam01@gmail.com>
2024-09-20 16:34:21 -0700

    Add files via upload

---
+++ send-email.js
@@ -0,0 +1,37 @@
+import nodemailer from "nodemailer"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+let transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.YOUREMAIL,
+    pass: process.env.APPPASSOWRD,
+  }
+});
+
+const mailoptions = {
+  from:{ 
+    name: "MY-company", 
+    address:"me" 
+  },
+  to: "recipient-gmail", // list of receivers
+  subject: "Hello world ✔", // Subject line
+  text: "Hello world?", // plain text body 
+  html: "<b>Hello world?</b>", // html body
+};
+
+const sendmail = async (transporter, mailoptions )=>{
+  try{
+await transporter.sendMail(mailoptions)
+console.log("message sended")
+  }
+  catch(err){
+    console.error(err)
+  }
+}
+sendmail(transporter,mailoptions)
